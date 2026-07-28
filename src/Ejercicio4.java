b755c5f43670371d172cc4df1790eb54a3d8d182
Nicolle Moreno <morenosancheznicollegabriela@gmail.com>
2026-07-27 20:33:28 -0500

    Se agrego el cuarto ejercicio

---
+++ src/Ejercicio4.java
@@ -0,0 +1,12 @@
+public class Ejercicio4 {
+        public static void main(String[] args) {
+
+            int numero = 5;
+
+            do {
+                System.out.println(numero);
+                numero += 5;
+            } while (numero <= 50);
+
+        }
+    }
